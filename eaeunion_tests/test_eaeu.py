292a9ce74c1b970306a395a3a8fa3e308dc692d9
Тимофеев Михаил Константинович <m.timofeev@ria.ru>
2024-08-07 16:15:46 +0300

    two tests

--- eaeunion_tests/test_eaeu.py
+++ eaeunion_tests/test_eaeu.py
@@ -6,6 +6,7 @@ sys.path.append('../')
 
 from pages.base import WebPage
 import time, re
+from datetime import date
 
 
 from selenium.webdriver.support.ui import WebDriverWait
@@ -45,10 +46,55 @@ class Test(Base):
                 # Проверяем, что указан ответственный департамент
                 assert "Ответственный департамент:" in l.text
                 # Проверяем подпись после даты
-                
+                flist = red.findall(l.text)
+                sstartdate = (flist[0][0]).split(".")
+                startdate = date(sstartdate[2], sstartdate[1], sstartdate[0])
+                senddate = (flist[0][1]).split(".")
+                enddate = date(senddate[2], senddate[1], senddate[0])
+                td = date.today()
+                if td == startdate:
+                    assert "Создан" in flist[0][2]
+                elif td > startdate and td <= enddate:
+                    assert "Идет обсуждение" in flist[0][2]
+                elif td > enddate:
+                    assert "Обсуждение завершено" in flist[0][2]
+                else:
+                    assert False # Непонятная дата обсуждения
             else:
-                # Проверяем этам разработки
+                # Проверяем этап разработки
                 assert "Общественное обсуждение" in l.text or "Оценка регулирующего воздействия" in l.text
+                ntr = 2
+
+    @allure.feature('Проверка списка на портале евразийского экономического союза')
+    @allure.description("Проверка списка документов по общественным обсуждениям и оценкам регулирующего воздействия\
+                        на портале евразийского экономического союза")    
+    @allure.story('Проверка фильтра по ответственному департаменту')
+    def test_eaeucheck(self, web_browser):
+        # pytest.skip("Временный скип для написания тестовых наборов")
+        self.init(web_browser)
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "//div[@class='cr-col-left']/div[@class='filters']\
+                                 /div[@data-name='npbdiscussiondepartmentresponsibletaxId']/a").click()
+        time.sleep(1)
+        divlist_el = self.driver.find_elements(By.XPATH, "//div[@id='ctl00_PlaceHolderMain_DeltaApplication1__scopeid']/div[@class='tree']/\
+                                               div/div")
+        for nel in range(1, len(divlist_el) + 1):
+            lbl = self.driver.find_element(By.XPATH, "//div[@id='ctl00_PlaceHolderMain_DeltaApplication1__scopeid']/div[@class='tree']/\
+                                               div/div[" + str(nel) + "]/label")
+            if "Департамент конкурентной политики и политики в области государственных закупок" in lbl.text:
+                lbl.click()
+                self.driver.find_element(By.XPATH, "//div[@id='ctl00_PlaceHolderMain_DeltaApplication1__scopeid']/div[@class='buttons']/\
+                                               /button").click()
+                break
+        time.sleep(5)
+        trlist_eae = self.driver.find_elements(By.XPATH, "//div[@class='discussionsAndRIA-panel']/table/tbody/tr")
+        # В одну строку должны попадать два элемента <tr>
+        ntr = 1
+        for l in trlist_eae:
+            if ntr == 2:
+                # Проверяем, что указан ответственный департамент
+                assert "Ответственный департамент: Департамент конкурентной политики и политики в области государственных закупок" in l.text
+            else:
                 ntr = 2
 
 
